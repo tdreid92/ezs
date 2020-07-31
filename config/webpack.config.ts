2860c49bc66f185e095a243ea4f7cf40cec79205
TylerReid <tyler.reid@sap.com>
2020-07-30 23:31:32 -0400

    Update webpack.config.ts

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,6 +1,9 @@
 import { CleanWebpackPlugin as CleanPlugin } from "clean-webpack-plugin";
 import TsCheckerPlugin from "fork-ts-checker-webpack-plugin";
 import { resolve } from "path";
+const AwsSamPlugin = require("aws-sam-webpack-plugin");
+
+const awsSamPlugin = new AwsSamPlugin();
 // const CleanPlugin = require(`pnp-webpack-plugin`);
 // const TsCheckerPlugin = require(`fork-ts-checker-webpack-plugin`);
 // const resolve = require(`path`);
@@ -11,8 +14,6 @@ import { Configuration } from "webpack";
 
 /** Name of the entry and output file. */
 const simpleEntryName = "file-example";
-/** Determine if this a production build. */
-const isProductionBuild: boolean = process.env.NODE_ENV == "production";
 /** Resolve paths down from the root directory. */
 
 const root = (pathToFile: string, filename?: string) =>
@@ -34,8 +35,8 @@ const paths = {
 /** The base webpack config needed with a few optimizations. Some of this should explain itself. */
 const baseConfig = {
   devtool: "source-map",
-  entry: paths.source.entry,
-  mode: isProductionBuild ? "production" : "development",
+  entry: () => awsSamPlugin.entry(),   // Loads the entry object from the AWS::Serverless::Function resources in your SAM config
+  mode: "production",
   module: {
     rules: [
       {
@@ -67,35 +68,20 @@ const baseConfig = {
     path: paths.build.root,
   },
   plugins: [
+    awsSamPlugin,
     new CleanPlugin(), // Clean the old files out on each run.
     new TsCheckerPlugin({
-      async: !isProductionBuild, // Only report after a run, freeing the process to work faster
       typescript: {
         build: true, // Build mode speeds up consequential builds (evertyhing after the first build, based on the prior build)
         configFile: paths.config.tsconfig,
         mode: "write-tsbuildinfo",
-        profile: !isProductionBuild, // Don't slow down production by profiling, only in development do we need this information.
       },
-    }),
-    {
-      apply: (compiler) => {
-        // This is a custom plugin (not imported) that tells Webpack to exit when done, since it tends to hang on CI servers.
-        compiler.hooks.done.tap("DonePlugin", (_stats) => {
-          if (isProductionBuild) {
-            setTimeout(() => {
-              console.log(`Compilation complete...`);
-              process.exit(0);
-            }, 0);
-          }
-        });
-      },
-    },
+    })
   ],
   stats: {
     warnings: false, // Turn off warnings, only express has warnings with Yarn 2 (future-proofing)
   },
   target: "async-node", // Target Node.js instead of web browsers.
-  watch: !isProductionBuild, // When in development, we watch for changes, otherwise compile and exit.
 };
 
 export default baseConfig;
