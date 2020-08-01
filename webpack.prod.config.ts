670da51e0b7f9014b4e1252367ee6101b0e7cdcd
TylerReid <tyler.reid@sap.com>
2020-08-01 01:07:58 -0400

    Add working prettier

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -1,43 +1,43 @@
-import TsCheckerPlugin from "fork-ts-checker-webpack-plugin";
-import { resolve } from "path";
-const AwsSamPlugin = require("aws-sam-webpack-plugin");
+import TsCheckerPlugin from 'fork-ts-checker-webpack-plugin';
+import { resolve } from 'path';
+import AwsSamPlugin from 'aws-sam-webpack-plugin';
 
 const awsSamPlugin = new AwsSamPlugin();
 
 /** The base webpack config needed with a few optimizations. Some of this should explain itself. */
 const baseConfig = {
-  devtool: "source-map",
-  entry: () => awsSamPlugin.entry(),   // Loads the entry object from the AWS::Serverless::Function resources in your SAM config
-  mode: "production",
+  devtool: 'source-map',
+  entry: () => awsSamPlugin.entry(), // Loads the entry object from the AWS::Serverless::Function resources in your SAM config
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.tsx?$/, // Look for ts or tsx files (future-proofing)
         use: [
           {
-            loader: "ts-loader", // We use ts-loader because Webpack doesn't understand TypeScript by default.
+            loader: 'ts-loader', // We use ts-loader because Webpack doesn't understand TypeScript by default.
             options: {
               configFile: resolve('tsconfig.json'), // Load our config file, not required, but should we move it, we have it already.
               transpileOnly: true, // Fork-TS-Checker-Webpack-Plugin does the type-checking for us.
               experimentalWatchApi: true,
               happyPackMode: true
-            },
-          },
-        ],
+            }
+          }
+        ]
       },
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
-        loader: "source-map-loader",
-      },
-    ],
+        loader: 'source-map-loader'
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: ['.ts', '.js']
   },
   output: {
     filename: (chunkData) => awsSamPlugin.filename(chunkData),
-    libraryTarget: "commonjs2",
+    libraryTarget: 'commonjs2',
     path: resolve('.') // CHECK
   },
   plugins: [
@@ -46,14 +46,14 @@ const baseConfig = {
       typescript: {
         build: true, // Build mode speeds up consequential builds (everything after the first build, based on the prior build)
         configFile: resolve('tsconfig.json'),
-        mode: "write-tsbuildinfo",
-      },
+        mode: 'write-tsbuildinfo'
+      }
     })
   ],
   // stats: {
   //   warnings: false, // Turn off warnings, only express has warnings with Yarn 2 (future-proofing)
   // },
-  target: "node", // Target Node.js instead of web browsers.
+  target: 'node' // Target Node.js instead of web browsers.
 };
 
 export default baseConfig;
